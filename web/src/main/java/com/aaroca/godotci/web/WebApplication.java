e164d86a1e1816ef9343430e61c70bd42d7c2be3
Álvaro Aroca <aaroca@redpoints.com>
2025-04-11 18:00:23 +0200

    Base web structure

---
+++ web/src/main/java/com/aaroca/godotci/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.aaroca.godotci.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
